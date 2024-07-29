f4e932e7f0c74cdc8a3e12b0e3225ca22219dc0c
Vitor Eble <vitoreble06@gmail.com>
2024-07-29 20:42:40 -0300

    componente card

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,18 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
   content: ["./src/**/*.{html,js}"],
+  darkMode: 'class',
   theme: {
-    extend: {},
+    extend: {
+      colors: {
+        brand: {
+          100: '#c542f5',
+          200: '#9e35c4',
+          300: '#6d2487',
+          400: '#331040',
+        }
+      }
+    },
   },
   plugins: [],
 }
